8bc067f9f2eb3f52e40b610131291f10528a0cdb
hlhr <56801493+hlhr@users.noreply.github.com>
2022-12-01 21:30:36 +0100

    day 01 with more linq

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,36 +1,16 @@
-﻿var inputLines = File.ReadLinesAsync("input.txt");
-
-var currentElfCalories = 0;
-
-var elves = new List<Elf>();
-
-await foreach (var line in inputLines)
+﻿var elvesTop3 = File.ReadAllLines("input.txt").Aggregate(new List<int> { 0 }, (elves, line) =>
 {
-    if (string.IsNullOrEmpty(line))
+    if (line == string.Empty)
     {
-        var elf = new Elf(elves.Count + 1, currentElfCalories);
-        elves.Add(elf);
-
-        currentElfCalories = 0;
-
-        continue;
+        elves.Add(0);
+    }
+    else
+    {
+        elves[^1] += int.Parse(line);
     }
 
-    currentElfCalories += int.Parse(line);
-}
+    return elves;
+}).OrderDescending().Take(3).ToList();
 
-var elvesCharts = elves.OrderDescending().ToList();
-
-Console.WriteLine($"Heavy loader: #{elvesCharts.First().Number}: {elvesCharts.First().Calories} Calories");
-Console.WriteLine($"Sum of top 3: {elvesCharts.Take(3).Sum(elf => elf.Calories)}");
-
-internal record Elf(int Number, int Calories) : IComparable<Elf>
-{
-    public readonly int Number = Number;
-    public readonly int Calories = Calories;
-
-    public int CompareTo(Elf? other)
-    {
-        return Calories.CompareTo(other?.Calories);
-    }
-}
+Console.WriteLine($"Top elf: {elvesTop3.First()} Calories");
+Console.WriteLine($"Sum of top 3: {elvesTop3.Sum()} Calories");
