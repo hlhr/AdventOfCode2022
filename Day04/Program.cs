12b5bc1f3cae916950ea4a05a2370f9c15392f5f
hlhr <56801493+hlhr@users.noreply.github.com>
2022-12-04 08:26:55 +0100

    day 04

---
+++ Day04/Program.cs
@@ -0,0 +1,23 @@
+﻿var elvesPairs = File.ReadAllLines("input.txt")
+    .Where(line => !string.IsNullOrEmpty(line))
+    .Select(line =>
+        {
+            return line
+                .Split(',')
+                .Select(sections => sections.Split('-')
+                    .Select(int.Parse)
+                    .ToArray())
+                .ToArray();
+        }
+    )
+    .ToList();
+
+var fullyContains = elvesPairs
+    .Count(elvesPair => (elvesPair[0][0] <= elvesPair[1][0] && elvesPair[0][1] >= elvesPair[1][1]) ||
+                        (elvesPair[1][0] <= elvesPair[0][0] && elvesPair[1][1] >= elvesPair[0][1]));
+
+var overlapping = elvesPairs
+    .Count(elvesPair => elvesPair[0][1] >= elvesPair[1][0] && elvesPair[1][1] >= elvesPair[0][0]);
+
+Console.WriteLine($"In how many assignment pairs does one range fully contain the other? {fullyContains}");
+Console.WriteLine($"In how many assignment pairs do the ranges overlap?? {overlapping}");
