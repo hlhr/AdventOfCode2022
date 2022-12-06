8d81bbee18513e8ae8beccaa3038521c55fe57c1
hlhr <56801493+hlhr@users.noreply.github.com>
2022-12-06 08:07:22 +0100

    day 06

---
+++ Day06/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetMarkerPosition(string input, int markerLength)
+{
+    for (var position = 0; position < input.Length; position++)
+    {
+        var marker = input.Substring(position, markerLength);
+
+        if (marker.Distinct().Count() == markerLength)
+        {
+            return position + markerLength;
+        }
+    }
+
+    return -1;
+}
+
+var signal = File.ReadAllText("input.txt");
+
+Console.WriteLine("How many characters need to be processed before the first start-of-packet marker is detected?");
+Console.WriteLine($"Marker 4: {GetMarkerPosition(signal, 4)}");
+Console.WriteLine($"Marker 14: {GetMarkerPosition(signal, 14)}");
