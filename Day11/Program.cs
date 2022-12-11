c71305eb42d401c6a9c6282176014f0335ec7a12
hlhr <56801493+hlhr@users.noreply.github.com>
2022-12-11 13:00:58 +0100

    day 11 part 2

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,83 +1,99 @@
-﻿var monkeys = File.ReadAllLines("input.txt")
-    .Where(line => !string.IsNullOrWhiteSpace(line))
-    .Chunk(6)
-    .Select(monkeyDefinition =>
-    {
-        var number = int.Parse(monkeyDefinition[0][7..^1]);
-        var items = monkeyDefinition[1][18..]
-            .Split(',', StringSplitOptions.TrimEntries)
-            .Select(int.Parse)
-            .ToList();
-        var throwCheckDivisibleBy = int.Parse(monkeyDefinition[3][21..]);
-        var positivCheckThrowTo = int.Parse(monkeyDefinition[4][29..]);
-        var negativCheckThrowTo = int.Parse(monkeyDefinition[5][29..]);
-
-        var operation = monkeyDefinition[2][19..];
-        Func<int, int> inspection;
-
-        if (operation == "old * old")
-        {
-            inspection = (old) => old * old;
-        }
-        else if (operation.StartsWith("old +"))
-        {
-            inspection = (old) => old + int.Parse(operation[6..]);
-        }
-        else if (operation.StartsWith("old *"))
-        {
-            inspection = (old) => old * int.Parse(operation[6..]);
-        }
-        else
-        {
-            throw new ArgumentOutOfRangeException();
-        }
-
-        return new Monkey
-        {
-            Number = number,
-            Items = items,
-            Inspection = inspection,
-            ThrowCheckDivisibleBy = throwCheckDivisibleBy,
-            PositivCheckThrowTo = positivCheckThrowTo,
-            NegativCheckThrowTo = negativCheckThrowTo
-        };
-    }).ToList();
-
-const int roundsToPlay = 20;
-
-for (var round = 0; round < roundsToPlay; round++)
+﻿List<Monkey> ParseMonkeys()
 {
-    foreach (var monkey in monkeys)
-    {
-        foreach (var item in monkey.Items)
+    return File.ReadAllLines("input.txt")
+        .Where(line => !string.IsNullOrWhiteSpace(line))
+        .Chunk(6)
+        .Select(monkeyDefinition =>
         {
-            var newItem = monkey.Inspection(item);
-            var newWorryLevel = newItem / 3;
-            var target = newWorryLevel % monkey.ThrowCheckDivisibleBy == 0
-                ? monkey.PositivCheckThrowTo
-                : monkey.NegativCheckThrowTo;
-            monkeys[target].Items.Add(newWorryLevel);
+            var items = monkeyDefinition[1][18..]
+                .Split(',', StringSplitOptions.TrimEntries)
+                .Select(long.Parse)
+                .ToList();
+            var throwCheckDivisibleBy = int.Parse(monkeyDefinition[3][21..]);
+            var positivCheckThrowTo = int.Parse(monkeyDefinition[4][29..]);
+            var negativCheckThrowTo = int.Parse(monkeyDefinition[5][29..]);
 
-            monkey.InspectedItemCount++;
-        }
+            var operation = monkeyDefinition[2][19..];
+            Func<long, long> inspection;
 
-        monkey.Items = new List<int>();
-    }
+            if (operation == "old * old")
+            {
+                inspection = (old) => old * old;
+            }
+            else if (operation.StartsWith("old +"))
+            {
+                inspection = (old) => old + int.Parse(operation[6..]);
+            }
+            else if (operation.StartsWith("old *"))
+            {
+                inspection = (old) => old * int.Parse(operation[6..]);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return new Monkey
+            {
+                Items = items,
+                Inspection = inspection,
+                ThrowCheckDivisibleBy = throwCheckDivisibleBy,
+                PositivCheckThrowTo = positivCheckThrowTo,
+                NegativCheckThrowTo = negativCheckThrowTo
+            };
+        }).ToList();
 }
 
-var topMonkeys = monkeys.Select(monkey => monkey.InspectedItemCount).OrderDescending().ToList();
-var monkeyBusiness = topMonkeys[0] * topMonkeys[1];
-Console.WriteLine($"What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans? {monkeyBusiness}");
+void Run(int roundsToPlay, List<Monkey> monkeys, Func<long, long> updateWorryLevel)
+{
+    for (var round = 0; round < roundsToPlay; round++)
+    {
+        foreach (var monkey in monkeys)
+        {
+            foreach (var item in monkey.Items)
+            {
+                var newItem = monkey.Inspection(item);
+                var newWorryLevel = updateWorryLevel(newItem);
+                var target = newWorryLevel % monkey.ThrowCheckDivisibleBy == 0
+                    ? monkey.PositivCheckThrowTo
+                    : monkey.NegativCheckThrowTo;
+                monkeys[target].Items.Add(newWorryLevel);
+
+                monkey.InspectedItemCount++;
+            }
+
+            monkey.Items.Clear();
+        }
+    }
+
+    var topMonkeys = monkeys.Select(monkey => monkey.InspectedItemCount).OrderDescending().Take(2).ToList();
+    var monkeyBusiness = topMonkeys[0] * topMonkeys[1];
+    Console.WriteLine($"What is the level of monkey business after {roundsToPlay} rounds of stuff-slinging simian shenanigans? {monkeyBusiness}");
+}
+
+void Part1()
+{
+    var monkeys = ParseMonkeys();
+    Run(20, monkeys, i => i / 3);
+}
+
+void Part2()
+{
+    var monkeys = ParseMonkeys();
+    var mod = monkeys.Aggregate(1, (total, current) => total * current.ThrowCheckDivisibleBy);
+    Run(10000, monkeys, i => i % mod);
+}
+
+Part1();
+Part2();
 
 internal class Monkey
 {
-    public required int Number { get; init; }
+    public required List<long> Items { get; init; }
 
-    public required List<int> Items { get; set; }
+    public long InspectedItemCount { get; set; }
 
-    public int InspectedItemCount { get; set; } = 0;
-
-    public required Func<int, int> Inspection { get; init; }
+    public required Func<long, long> Inspection { get; init; }
 
     public required int ThrowCheckDivisibleBy { get; init; }
 
